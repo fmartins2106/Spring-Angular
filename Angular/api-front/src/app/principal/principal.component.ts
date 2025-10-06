5df0be31e4f03aa38071443e7e4b1318833ad347
Fernando Martins <fernandom.adm@gmail.com>
2025-10-06 10:36:53 -0300

    Adicionando formulario

--- Angular/api-front/src/app/principal/principal.component.ts
+++ Angular/api-front/src/app/principal/principal.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+
 
 @Component({
   selector: 'app-principal',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.css'
 })
 export class PrincipalComponent {
+
+  // Variável para visibilidade dos botões
+  btnCadastro:boolean = true;
 
 }
